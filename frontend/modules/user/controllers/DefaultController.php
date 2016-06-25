1e703444194fa22a5a2cc8ffcd38408af2c396d5
Tinywan <756684177@qq.com>
2016-06-25 13:11:58 +0800

    1233123

---
+++ frontend/modules/user/controllers/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace frontend\modules\user\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        echo 'user Moduels';
+        //return $this->render('index');
+    }
+}

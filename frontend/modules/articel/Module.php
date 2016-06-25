a7156caa2f3a816f6f754f63f996d5792a64074b
Tinywan <756684177@qq.com>
2016-06-25 13:25:31 +0800

    使用模块写一个API接口，RESTful APIs

---
+++ frontend/modules/articel/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\articel;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\articel\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ frontend/modules/articel/controllers/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace frontend\modules\articel\controllers;
+
+use yii\web\Controller;
+
+class DefaultController extends Controller
+{
+    public function actionIndex()
+    {
+        echo '11111111111';
+        //return $this->render('index');
+    }
+}

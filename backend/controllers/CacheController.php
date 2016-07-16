988e6e786f873ab56f96c0bc552546117a79811d
Tinywan <756684177@qq.com>
2016-07-16 10:47:04 +0800

    Yii 数据缓存 1.cache 默认缓存

---
+++ backend/controllers/CacheController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tinywan
+ * Date: 2016/5/22
+ * Time: 18:22
+ */
+namespace backend\controllers;
+
+use Yii;
+//use yii\web\Controller;
+use yii\rest\Controller;
+use backend\models\Student;
+
+/**
+ * CameraController implements the CRUD actions for Camera model.
+ */
+class CacheController extends Controller
+{
+   public function actionIndex(){
+      echo '22222222222222';
+   }
+}

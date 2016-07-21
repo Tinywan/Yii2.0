0875e5fcb0b36d43765a0e1b7fc2c9a0fc22ac1d
Tinywan <756684177@qq.com>
2016-07-21 20:19:58 +0800

    添加两个测试文件

---
+++ backend/controllers/RedisController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+//use yii\web\Controller;
+use yii\rest\Controller;
+use backend\models\Student;
+
+/**
+ * CameraController implements the CRUD actions for Camera model.
+ */
+class RedisController extends Controller
+{
+   public function actionIndex(){
+       $student = new Student();
+       $student->attributes = ['name'=>'Tinywan'];
+       if($student->save() == false){
+           echo 'save';
+       }
+       echo $student->id;
+
+       $result= Student::find()->where(['name' => 'Tinywan'])->one();
+       var_dump($result);
+       //$student = Student::find()->active()->all();
+   }
+}

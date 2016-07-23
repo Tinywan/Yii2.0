47739242b68016a92fffede130f58c026bc34445
Tinywan <756684177@qq.com>
2016-07-23 10:32:49 +0800

    csrf攻击之yii验证过程

---
+++ backend/controllers/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+
+
+class SecurityController extends Controller
+{
+    /**
+     * Csrf 的Post请求测试
+     */
+    public function actionCsrf()
+    {
+
+        if(Yii::$app->request->isPost){
+            echo Yii::$app->request->post('title');
+        }else{
+            /**
+             * 获取_csrf 的值
+             * 1，这个值发送给客户端的时候回保存两份
+             *    【1】保存在客户端本地的cookie中（是经过算法加密的）
+             *    【2】一份在一个隐藏的表单中_csrf 跟随表单提交
+             * 2，服务器接收到表单提交和cookie提交的csrf值，
+             *    【1】首先会解密cookie中的csrf的值
+             *    【2】和表单中提交的csrf的值进行比较
+             */
+            $csrfToken = Yii::$app->request->csrfToken;
+            return $this->renderPartial('csrf',['csrfToken'=>$csrfToken]);
+        }
+
+    }
+}
---
+++ backend/views/security/csrf.php
@@ -0,0 +1,5 @@
+<form method="post">
+    <input type="text" name="title" value="hello"/>
+    <input type="hidden" name="_csrf" value="<?=$csrfToken?>"/>
+    <input type="submit"  value="submit"/>
+</form>

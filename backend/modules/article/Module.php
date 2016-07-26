bc6ea2ce43edd2b14b3916fa9309e34014652cb1
Tinywan <756684177@qq.com>
2016-07-26 14:24:48 +0800

    新增加一个article模块

--- backend/modules/article/Module.php
+++ backend/modules/article/Module.php
@@ -4,6 +4,7 @@ namespace backend\modules\article;
 
 class Module extends \yii\base\Module
 {
+    //如果一些控制器不再该命名空间下，可配置yii\base\Module::controllerMap属性让它们能被访问
     public $controllerNamespace = 'backend\modules\article\controllers';
 
     public function init()
